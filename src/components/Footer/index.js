95f02143497c1cf5c019d79b11887984ed51d7ee
sherin2022 <sherinj@maveric-systems.com>
2022-03-24 14:21:00 +0530

    change logo color

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,7 +6,7 @@ import FooterLogo from '../../images/maveric-logo-white.png'
 export default function Footer(){
     return(
     <MDBFooter bgColor='light' className='text-center text-lg-left'>
-      <div className='text-center p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
+      <div className='text-center p-3' style={{ backgroundColor: 'rgba(28, 68, 230, 0.85)' }}>
         <img src = {FooterLogo}  alt= "Footer Image"></img>       
         &copy; {new Date().getFullYear()} Copyright:{' '} 
         Maveric Systems Limited
