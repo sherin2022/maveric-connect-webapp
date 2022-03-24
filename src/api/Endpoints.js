177721944d7af736ed5ff3eebfa44be7ef15800a
Savita Golabavi <savitago@maveric-systems.com>
2022-03-24 17:37:24 +0530

    login page commited

---
+++ src/api/Endpoints.js
@@ -0,0 +1,7 @@
+const Endpoints = {
+    LOGIN_URL:'',
+    SIGNUP_URL:'',
+    POST_LIKE_URL: '',
+    POST_DETAILS_URL: ''
+}
+export default Endpoints;
---
+++ src/components/Footer/index.js
@@ -0,0 +1,25 @@
+ {/* import React from 'react';
+import { MDBFooter } from 'mdb-react-ui-kit';
+import FooterLogo from '../../images/maveric-logo-white.png'
+  export default function Footer(){
+    const headerStyle =   {
+        backgroundColor: 'rgba(33, 78, 143, 1)',
+
+    }
+    return(
+        
+   <MDBFooter bgColor='light' className='text-center text-lg-left'>
+      <div className='text-center p-3' style={headerStyle}>
+        <img src = {FooterLogo}  alt= "Footer Image"></img>
+        <div style={{color:'white'}}>      
+        &copy; {new Date().getFullYear()} Copyright:{' '} 
+        Maveric Systems Limited
+        </div>
+      </div>
+    </MDBFooter>
+   
+    )
+   
+    
+}
+ */}
