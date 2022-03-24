6e19ad2a0dc91c71e730e1391a122030b81eb2b1
sherin2022 <sherinj@maveric-systems.com>
2022-03-24 14:58:37 +0530

    add the brand color to the footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,12 +4,19 @@ import FooterLogo from '../../images/maveric-logo-white.png'
 
 
 export default function Footer(){
+    const headerStyle =   {
+        backgroundColor: 'rgba(33, 78, 143, 1)',
+
+    }
     return(
+        
     <MDBFooter bgColor='light' className='text-center text-lg-left'>
-      <div className='text-center p-3' style={{ backgroundColor: 'rgba(28, 68, 230, 0.85)' }}>
-        <img src = {FooterLogo}  alt= "Footer Image"></img>       
+      <div className='text-center p-3' style={headerStyle}>
+        <img src = {FooterLogo}  alt= "Footer Image"></img>
+        <div style={{color:'white'}}>      
         &copy; {new Date().getFullYear()} Copyright:{' '} 
         Maveric Systems Limited
+        </div>
       </div>
     </MDBFooter>
     )
