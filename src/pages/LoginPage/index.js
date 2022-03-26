9ad81d7036a65cf1f7873ed1f7f26899cb9b5e8e
sherin2022 <sherinj@maveric-systems.com>
2022-03-27 05:28:40 +0530

    add fetch api for post list

---
+++ src/pages/LoginPage/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../../components/Header'
+import Footer from '../../components/Footer'
+const LoginPage = () =>{
+    return(
+        <div>
+           <Header />
+           Add loginpage content by Savita 
+           <Footer />
+        </div>
+    )
+}
+
+export default LoginPage;
